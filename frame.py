31175cbb2a61acc65e48c2b0a6a7e832c825155c
MoreHotSauce <32422309+MoreHotSauce@users.noreply.github.com>
2018-09-05 10:18:27 -0700

    fixed crash when no objects were found

--- frame.py
+++ frame.py
@@ -7,7 +7,9 @@ import cv2
 iYolo = YOLO()
 
 #get middle of the first found stop sign
-def middle(boxes, output): 
+def middle(boxes, output):
+    if !np.where(output==11):
+        return (False, False)
     signIndex = np.where(output==11)
     return [((boxes[signIndex][0][0] + boxes[signIndex][0][2]) / 2), ((boxes[signIndex][0][1] + boxes[signIndex][0][3]) / 2)]
 #distance formula
@@ -18,6 +20,8 @@ def getTotalDistance(image):
     image = image.resize((100, 100))
     boxes, labels = iYolo.detect_image(image)
     y, x = middle(boxes, labels)
+    if !y:
+      return 50
     return distance(50, 50, x, y)
 
 
